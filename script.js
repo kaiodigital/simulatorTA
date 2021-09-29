553f5996bf08f850071e5298b649a6b25fd547cb
Kaio ARAUJO <kaiopeyron@gmail.com>
2021-09-29 15:43:05 +0200

    comming replace NaN for 0 value

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 function calculer() {
-  array = document.querySelector("input");
+  array = document.querySelector("#masseSalariale");
   for (I = 0; I < array.length; I++) {
     if (array[I].value.length == 0) array[I].value = 0;
-    if (isNaN(array[I].value)) {
-      alert(array[I].value + " Ce n'est pas un nombre !");
-      break;
-    }
+    // if (isNaN(array[I].value)) {
+    //   alert(array[I].value + " Ce n'est pas un nombre !");
+    //   break;
+    // }
   }
 
   if (I < array.length) return;
-  TA =
-    parseFloat(masseSalariale.value) *
-    parseFloat(alsace.value);
-    TA * 0.013;
+  TA = parseFloat(masseSalariale.value) * parseFloat(alsace.value);
+  TA * 0.013;
 
   const totalTA = (resultatTA.innerText = TA.toFixed(0) + " €"); // Taxe apprentissage resultat
   console.log("totalTA : " + totalTA); // affichage total TA
 
   const totalSolde = (resultatSolde.innerText =
-    ((parseInt(totalTA) * 13) / 100).toFixed(0) + " €");
+    ((parseInt(totalTA) * 13) / 100).toFixed(0) + " €"); // Solde resultat
   console.log("totalSolde : " + totalSolde); // Affichage total du solde (13%)
 
   const totalFinancement = (resultatFinancement.innerText =
-    ((parseInt(totalTA) * 87) / 100).toFixed(0) + " €");
+    ((parseInt(totalTA) * 87) / 100).toFixed(0) + " €"); // total financement resultat
   console.log("totalFinancement : " + totalFinancement); // Affichage total du financement (87%)
+
 }
 
 const alsace = document.querySelector("#alsaceValue");
@@ -40,3 +39,13 @@ function changeValue() {
     console.log("unchecked");
   }
 }
+
+function mykeyUp() {
+  document.querySelector("#masseSalariale");
+  // getZero()
+  calculer();
+}
+
+// function getZero() {
+//   if (isNaN(resultatTA)) resultatTA = "0 €";
+// }
