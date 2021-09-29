6d0ea50514f34502c00eb061f5ed4494e278f485
Kaio ARAUJO <kaiopeyron@gmail.com>
2021-09-29 14:30:15 +0200

    bug calcul + design

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculer() {
   for (I = 0; I < array.length; I++) {
     if (array[I].value.length == 0) array[I].value = 0;
     if (isNaN(array[I].value)) {
-      alert(array[I].value + " n'est pas numérique !");
+      alert(array[I].value + " Ce n'est pas un nombre !");
       break;
     }
   }
@@ -11,28 +11,32 @@ function calculer() {
   if (I < array.length) return;
   TA =
     parseFloat(masseSalariale.value) *
-    parseFloat(alsace.value) * // add variable pocur Alsace))
-    0.13;
+    parseFloat(alsace.value);
+    TA * 0.013;
 
-  const totalTA = resultatTA.innerText = TA.toFixed(2) + " €"; // Taxe apprentissage resultat
-  console.log('totalTA : ' + totalTA); // affichage total TA
+  const totalTA = (resultatTA.innerText = TA.toFixed(0) + " €"); // Taxe apprentissage resultat
+  console.log("totalTA : " + totalTA); // affichage total TA
 
-  const totalSolde = resultatSolde.innerText = ((parseInt(totalTA) * 13) / 100).toFixed(2) + " €";
-  console.log('totalSolde : ' + totalSolde); // Affichage total du solde (13%)
+  const totalSolde = (resultatSolde.innerText =
+    ((parseInt(totalTA) * 13) / 100).toFixed(0) + " €");
+  console.log("totalSolde : " + totalSolde); // Affichage total du solde (13%)
 
-  const totalFinancement = resultatFinancement.innerText = ((parseInt(totalTA) * 87) / 100).toFixed(2) + " €";
-  console.log('totalFinancement : ' + totalFinancement); // Affichage total du financement (87%)
-
+  const totalFinancement = (resultatFinancement.innerText =
+    ((parseInt(totalTA) * 87) / 100).toFixed(0) + " €");
+  console.log("totalFinancement : " + totalFinancement); // Affichage total du financement (87%)
 }
 
 const alsace = document.querySelector("#alsaceValue");
 
 function changeValue() {
+  // Checked for alsace value
   if (alsace.checked) {
-    alsace.setAttribute("value", "0.44");
+    alsace.setAttribute("value", "0.0044");
+    calculer();
     console.log("checked");
   } else {
-    alsace.setAttribute("value", "0.68");
+    alsace.setAttribute("value", "0.0068");
+    calculer();
     console.log("unchecked");
   }
 }
